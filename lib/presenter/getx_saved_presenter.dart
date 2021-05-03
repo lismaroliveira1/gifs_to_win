6dcacfdc6139658a26fd6cbb8e11cc056979a7ac
lismaroliveira1 <englismaroliveira@gmail.com>
2021-05-03 18:40:31 -0300

    add GetXTrashPresenter

---
+++ lib/presenter/getx_saved_presenter.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+import '../models/model.dart';
+
+class GetXTrashPresenter extends GetxController {
+  final ImageRepository repository;
+  GetXTrashPresenter({@required this.repository});
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+}
