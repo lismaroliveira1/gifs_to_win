949e37d0a413f94791d871ffb031b65d0c539324
lismaroliveira1 <englismaroliveira@gmail.com>
2021-05-08 17:35:30 -0300

    add inner_drawer on GifSavedPage

--- lib/view/gif_saved_page/gif_saved_page.dart
+++ lib/view/gif_saved_page/gif_saved_page.dart
@@ -16,62 +16,71 @@ class GifSavedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(
-      () => Scaffold(
-        appBar: buildAppBar(
-          context: context,
-          innerDrawerKey: _innerDrawerKey,
-          initialValue: presenter.wayViewModeOut,
-          buttonCallback: presenter.changeWayViewMode,
-          title: 'GifsSalvos',
-        ),
-        drawer: CurstomDrawer(presenter.jumpToPage),
-        body: GestureDetector(
-          onTap: () => hideKeyboard(context: context),
-          child: Builder(
-            builder: (context) {
-              presenter.jumpToStream.listen((page) {
-                if (page?.isNotEmpty == true) {
-                  Get.offAllNamed(page);
-                }
-              });
-              return Obx(
-                () => presenter.isLoadingOut
-                    ? Container(
-                        child: Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                      )
-                    : Column(
-                        children: [
-                          buildForm(
-                            onChanged: presenter.validateSearchName,
-                            errorText: presenter.errorTextDialogStream,
-                            controller: _controller,
-                            onSubmited: presenter.onSubmited,
-                            labelText: 'Pesquisar',
-                            hintText: '',
-                            icon: Icons.search,
-                          ),
-                          Expanded(
-                            child: buildImageListView(
-                              context: context,
-                              getMoreImages: () async {},
-                              imageList: presenter.imageSavedLisMapOut,
-                              isSearch: false,
-                              searchName: '',
-                              closeCallback: () {},
-                              moveToBlakiList: presenter.moveToBlakiList,
-                              editImageTitleDialog: presenter.showEditDialog,
-                              deleteImage: (Map<dynamic, dynamic> imageMap) {},
-                              saveImage: (Map<dynamic, dynamic> imageMap) {},
-                              shareImage: (Map<dynamic, dynamic> imageMap) {},
-                            ),
-                          ),
-                        ],
+      () => buildCustomDrawer(
+        routePageCallBack: presenter.jumpToPage,
+        context: context,
+        key: _innerDrawerKey,
+        scaffold: buildScaffold(context),
+      ),
+    );
+  }
+
+  Scaffold buildScaffold(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(
+        context: context,
+        innerDrawerKey: _innerDrawerKey,
+        initialValue: presenter.wayViewModeOut,
+        buttonCallback: presenter.changeWayViewMode,
+        title: 'GifsSalvos',
+      ),
+      drawer: CurstomDrawer(presenter.jumpToPage),
+      body: GestureDetector(
+        onTap: () => hideKeyboard(context: context),
+        child: Builder(
+          builder: (context) {
+            presenter.jumpToStream.listen((page) {
+              if (page?.isNotEmpty == true) {
+                Get.offAllNamed(page);
+              }
+            });
+            return Obx(
+              () => presenter.isLoadingOut
+                  ? Container(
+                      child: Center(
+                        child: CircularProgressIndicator(),
                       ),
-              );
-            },
-          ),
+                    )
+                  : Column(
+                      children: [
+                        buildForm(
+                          onChanged: presenter.validateSearchName,
+                          errorText: presenter.errorTextDialogStream,
+                          controller: _controller,
+                          onSubmited: presenter.onSubmited,
+                          labelText: 'Pesquisar',
+                          hintText: '',
+                          icon: Icons.search,
+                        ),
+                        Expanded(
+                          child: buildImageListView(
+                            context: context,
+                            getMoreImages: () async {},
+                            imageList: presenter.imageSavedLisMapOut,
+                            isSearch: false,
+                            searchName: '',
+                            closeCallback: () {},
+                            moveToBlakiList: presenter.moveToBlakiList,
+                            editImageTitleDialog: presenter.showEditDialog,
+                            deleteImage: (Map<dynamic, dynamic> imageMap) {},
+                            saveImage: (Map<dynamic, dynamic> imageMap) {},
+                            shareImage: (Map<dynamic, dynamic> imageMap) {},
+                          ),
+                        ),
+                      ],
+                    ),
+            );
+          },
         ),
       ),
     );
