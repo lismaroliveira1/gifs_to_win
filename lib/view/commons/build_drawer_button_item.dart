2a73900486672819b93c537306818b307dd63181
lismaroliveira1 <englismaroliveira@gmail.com>
2021-05-08 18:02:30 -0300

    mode buildDrawerItem method to its own file

--- lib/view/commons/build_drawer_button_item.dart
+++ lib/view/commons/build_drawer_button_item.dart
@@ -1,46 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_inner_drawer/inner_drawer.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import './commons.dart';
-
-Widget buildCustomDrawer({
-  @required BuildContext context,
-  @required Function(String page) routePageCallBack,
-  @required Scaffold scaffold,
-  @required GlobalKey key,
-}) {
-  double _edge = MediaQuery.of(context).size.width;
-
-  return InnerDrawer(
-    key: key,
-    onTapClose: true,
-    swipe: true,
-    colorTransitionScaffold: Colors.black54,
-    offset: IDOffset.only(bottom: 0.05, right: 0.0, left: 0.0),
-    scale: IDOffset.horizontal(0.8),
-    proportionalChildArea: true,
-    borderRadius: 50,
-    leftAnimationType: InnerDrawerAnimation.static,
-    rightAnimationType: InnerDrawerAnimation.quadratic,
-    onDragUpdate: (double val, InnerDrawerDirection direction) {},
-    innerDrawerCallback: (a) {},
-    leftChild: buildLeftChildDrawer(
-      edge: _edge,
-      routePageCallBack: routePageCallBack,
-      context: context,
-      reverse: true,
-    ),
-    rightChild: buildRIghtChildDrawer(
-      edge: _edge,
-      routePageCallBack: routePageCallBack,
-      context: context,
-      reverse: false,
-    ),
-    backgroundDecoration: backgroundDecoration(),
-    scaffold: scaffold,
-  );
-}
 
 Widget buildDrawerButtonItem({
   @required String item,
