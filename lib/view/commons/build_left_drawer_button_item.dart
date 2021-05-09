1e90311c4fea604f38e62a5e945127c41d035b1c
lismaroliveira1 <englismaroliveira@gmail.com>
2021-05-09 11:56:15 -0300

    move buuldLeftDrawerItem to its own file

---
+++ lib/view/commons/build_left_drawer_button_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Widget buildLeftDrawerButtonItem({
+  @required String item,
+  @required BuildContext context,
+  @required Function(String page) callback,
+  @required IconData icon,
+  @required String page,
+  @required bool reverse,
+}) {
+  return InkWell(
+    onTap: () {
+      Navigator.of(context).pop();
+      Future.delayed(
+        Duration(milliseconds: 500),
+        callback(page),
+      );
+    },
+    child: Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: reverse
+          ? Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: Text(
+                    item,
+                    style: GoogleFonts.itim(
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                Icon(icon)
+              ],
+            )
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(icon),
+                Container(
+                  child: Text(
+                    item,
+                    style: GoogleFonts.itim(
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+    ),
+  );
+}
