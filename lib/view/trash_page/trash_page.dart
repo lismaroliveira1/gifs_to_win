11db650925c3e95573bee9e5f3a5fb7a226a53ca
lismaroliveira1 <englismaroliveira@gmail.com>
2021-05-20 16:29:34 -0300

    first  release

--- lib/view/trash_page/trash_page.dart
+++ lib/view/trash_page/trash_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inner_drawer/inner_drawer.dart';
 
 import 'package:get/get.dart';
-import 'package:gipher/i18n/i18n.dart';
 
+import '../../i18n/i18n.dart';
 import '../../presenter/presenter.dart';
 import '../view.dart';
 
